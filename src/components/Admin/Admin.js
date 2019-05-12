2a1153a907a49ff02818ac94231c66763f321217
Rachel Schoenmann <rachel.schoenmann@gmail.com>
2019-05-11 20:48:04 -0500

    all fields in GET request now appending to DOM correctly

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { WithStyles, lighten, Button, Table, TableBody, TableCell, TableHead, TablePagination, TableRow, TableSortLabel, Toolbar, Checkbox, IconButton, DeleteIcon, Tooltip, FilterListIcon } from '@material-ui/core';
+import {lighten, Button, Table, TableBody, TableCell, TableHead, TablePagination, TableRow, TableSortLabel, Toolbar, Checkbox, IconButton, DeleteIcon, Tooltip, FilterListIcon } from '@material-ui/core';
 import axios from 'axios';
+import AdminData from '../AdminData/AdminData';
 
 class Admin extends Component {
-
-	state = {
-		allFeedback: [],
-	}
 
 	componentDidMount(){
 		console.log('componentDidMount');
@@ -17,15 +14,14 @@ class Admin extends Component {
 	getFeedback = () => {
 		axios.get('/feedback').then(response => {
 			console.log('response.data:', response.data);
-			this.setState({
-				allFeedback: response.data
-			})
+			this.props.dispatch({type: 'GET_FEEDBACK', payload: response.data})
 		}).catch(error => {
 			console.log('error in GET:', error);
 		})
 	};//end getFeedback
 
 	render(){
+		// make sure we're getting the data we're expecting: console.log('this.props.reduxState.getFeedbackReducer:', this.props.reduxState.getFeedbackReducer)
 		return(
 			<div>
 				<h2>Admin Page</h2>
@@ -45,7 +41,11 @@ class Admin extends Component {
 								</TableRow>
 							</TableHead>
 							<TableBody>
-
+								{this.props.entry.map((anEntry) => {
+									return (
+										<AdminData key={anEntry.id} anEntry={anEntry} />
+									)
+								})}
 							</TableBody>
 						</Table>
 					</div>
@@ -56,7 +56,8 @@ class Admin extends Component {
 
 const mapReduxStateToProps = (reduxState) => {
 	return {
-		reduxState
+		//set reduxState.getFeedbackReducer to entry so we don't have to type reduxState.getFeedbackReducer everytime....
+		entry: reduxState.getFeedbackReducer
 	}
 }
 
